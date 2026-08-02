ceb0913de80e7e873fa1e1552e10a9a7bd3a4938
Sakhawat Hossain <sajuislam266@gmail.com>
2026-08-02 11:29:07 +0600

    test done

--- app/Http/Controllers/Api/Auth/UserController.php
+++ app/Http/Controllers/Api/Auth/UserController.php
@@ -23,15 +23,8 @@ class UserController extends Controller
         $this->select = ['id', 'name', 'email', 'avatar', 'last_activity_at'];
     }
 
-    public function me(Request $request)
+    public function me()
     {
-
-    dd([
-        'bearer' => $request->bearerToken(),
-        'authorization' => $request->header('Authorization'),
-        'guard_check' => auth('api')->check(),
-        'user' => auth('api')->user(),
-    ]);
 
         $vehicle=$this->user->vehicles()->count();
         $post=$this->user->posts()->count();
