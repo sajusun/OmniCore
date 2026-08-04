1ab0d13b1eb83ca400fe402efe4e6860c80f0f3d
Sakhawat Hossain <sajuislam266@gmail.com>
2026-08-04 16:26:18 +0600

    add vehicle delete option

--- routes/api.php
+++ routes/api.php
@@ -96,6 +96,7 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/vehicles/store', [VehicleController::class, 'store']);
     Route::get('/vehicles/{vehicle}/show', [VehicleController::class, 'show']);
     Route::post('/vehicles/{vehicle}/update', [VehicleController::class, 'update']);
+    Route::post('/vehicles/{vehicle}/delete', [VehicleController::class, 'destroy']);
     Route::get('/vehicles/meta', [VehicleController::class, 'meta']);
 });
 
